9721b3bd32d01d8f9e1fb8de7a87d7332b7b7efb
gabryelboeira <gabryel392@gmail.com>
2022-06-19 17:46:21 -0300

    Layout dos da tela de cadastro do app e a implementacao no main

--- ReservaVeicular/app/src/main/java/com/br/utfpr/gabryel/reservaveicular/MainActivity.java
+++ ReservaVeicular/app/src/main/java/com/br/utfpr/gabryel/reservaveicular/MainActivity.java
@@ -1,14 +1,40 @@
 package com.br.utfpr.gabryel.reservaveicular;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import com.br.utfpr.gabryel.reservaveicular.fragment.DatePickerFragment;
+import com.br.utfpr.gabryel.reservaveicular.model.enums.TipoCnh;
+import com.br.utfpr.gabryel.reservaveicular.utils.MontarUtils;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinnerCnh;
+    private final MontarUtils montarUtils;
+
+    public MainActivity() {
+        this.montarUtils = new MontarUtils(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinnerCnh = findViewById(R.id.spinnerCnh);
+        spinnerCnh.setAdapter(montarUtils.carregarDadosSpinner(TipoCnh.descricaoList()));
     }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment(findViewById(R.id.editDtNascimento));
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+
 }
