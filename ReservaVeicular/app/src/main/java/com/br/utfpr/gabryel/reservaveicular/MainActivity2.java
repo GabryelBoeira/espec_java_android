a22fdde74dff155e74b5bc0c4d7c73084d1a0bee
gabryelboeira <gabryel392@gmail.com>
2022-06-20 00:40:41 -0300

    utilizando listview

--- ReservaVeicular/app/src/main/java/com/br/utfpr/gabryel/reservaveicular/MainActivity2.java
+++ ReservaVeicular/app/src/main/java/com/br/utfpr/gabryel/reservaveicular/MainActivity2.java
@@ -1,14 +1,20 @@
 package com.br.utfpr.gabryel.reservaveicular;
 
+import android.os.Bundle;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity2 extends AppCompatActivity {
+
+    private ListView listViewMotoristas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        listViewMotoristas = findViewById(R.id.listViewMotorista);
+
     }
 }
